93b68a8afa7e00d36e44ab9c73f25cc3e41d080f
damir.hasanshin <d.xasanshin@bk.ru>
2023-12-08 18:24:21 +0300

    add controller

---
+++ Core/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Core;
+
+/**
+ * BaseController.
+ */
+class Controller
+{
+    protected $layout = "default";
+    protected $title = "";
+
+    /**
+     * 
+     */
+    protected function render($view, $data) 
+    {
+        return new Page($this->layout, $this->title, $view, $data);
+    }
+}
