69ff0d5a8c2bfe81ea5d7abaadec8732e3d91a85
Damir.Hasanshin <damir.hasanshin@tatar.ru>
2024-04-07 17:12:10 +0300

    update gitignore

--- config/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-// for example DB_DSN = "mysql:host=localhost;dbname=test"
-define("DB_DSN", "mysql:host=localhost;dbname=mild");
-define("DB_USER", "damirhasanshin");
-define("DB_PASSWORD", "1122");
